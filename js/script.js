bb79e3f0682d61564e2f8f563907a1e1febc77ce
viv <vivyanw@uw.edu>
2014-10-16 10:04:56 -0700

    did demo stuff

--- js/script.js
+++ js/script.js
@@ -5,3 +5,38 @@
 	Javascript 
 	Comment
 */
+"use strict";
+
+var person = {
+	fName: 'Viv',
+	lName: 'W',
+	email: 'type here',
+	getFullName: function() { return this.fName + ' ' + this.lName}
+};
+
+//NEVER EVER use alert
+//alert(person.getFullName());
+console.log('running the getFullName function on person');
+console.log(person.getFullName());
+
+console.log('looping through all properties of person');
+//show all properties of the person object
+for (var prop in person) {
+	console.log(person[prop]);
+};
+
+//create a new image element
+var elem = document.createElement('img');
+elem.src = 'img/cat.jpg';
+elem.alt = 'picture of an awesome and adorable cat';
+
+//add this element to the top of the body
+document.body.insertBefore(elem, document.body.firstChild);
+
+//create new element again
+var dog = document.createElement('img');
+dog.src = 'img/dog.jpg';
+dog.alt = 'picture of a cute puppy';
+
+//add this to the body of the page
+document.body.appendChild(dog);
